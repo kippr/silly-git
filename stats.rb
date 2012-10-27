7a58be6fedad08813e4c7f5ca583c5d78710605f
Kippr <github@krispowell.net>
2012-10-27 21:28:51 +0100

    Drop into repl for introspection

--- stats.rb
+++ stats.rb
@@ -1,10 +1,37 @@
 
 commits = `git log --numstat | grep -E '^[0-9]+[[:space:]]+[0-9]+'`
 
-per_file = Hash.new([0,0,0])
+class FileStat
+    def initialize file_name
+        @file_name = file_name
+        @count, @added, @removed = 0, 0, 0
+    end
+
+    def record added, removed
+        @count += 1
+        @added += added
+        @removed += removed
+    end
+
+    def to_s
+        "#{@file_name}: #{@count} commit#{@count != 1 ? 's' : ''}, #{@added} #{@removed}"
+    end
+
+end
+
+
+per_file = Hash.new{|h,file_name| h[file_name] = FileStat.new(file_name) }
+total = FileStat.new('Total')
 
 commits.each_line do | line |
     added, removed, file = line.split
-    per_file[file] = per_file[file].zip([added.to_i, removed.to_i, 1]).map{|a,b|a+b}
+    added, removed = added.to_i, removed.to_i
+    per_file[file].record(added, removed)
+    total.record(added, removed)
 end
-puts per_file
+
+num_files = per_file.size
+
+
+require 'pry'
+binding.pry
